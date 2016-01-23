767f7d425978f08f25643c0bb8b2ef29708fb4d7
dewafer <dewafer@gmail.com>
2016-01-23 14:38:37 +0800

    add a page lists all the images

--- src/main/java/wang/yinqiu/docker/captain/controller/MainController.java
+++ src/main/java/wang/yinqiu/docker/captain/controller/MainController.java
@@ -3,6 +3,7 @@ package wang.yinqiu.docker.captain.controller;
 import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.DockerException;
 import com.spotify.docker.client.messages.Container;
+import com.spotify.docker.client.messages.Image;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,5 +26,13 @@ public class MainController {
         List<Container> containers = dockerClient.listContainers(DockerClient.ListContainersParam.allContainers());
         model.addAttribute("containers", containers);
         return "index";
+    }
+
+    @RequestMapping("images.html")
+    public String listImagesPage(Model model) throws DockerException, InterruptedException {
+        // docker images -a
+        List<Image> images = dockerClient.listImages(DockerClient.ListImagesParam.allImages());
+        model.addAttribute("images", images);
+        return "images";
     }
 }
